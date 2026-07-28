4ff48600b00fbd355ebea08cef7694ea0c0ceed5
cindyyawencheng <ccheng623@gmail.com>
2026-07-28 20:21:58 +0200

    allow for slideshow in performances and attribution in location

--- astroship/src/content/config.ts
+++ astroship/src/content/config.ts
@@ -1,19 +1,32 @@
 import { z, defineCollection } from "astro:content";
 
 const performanceCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      draft: z.boolean(),
+      title: z.string(),
+      snippet: z.string(),
+
+      image: z.object({
+        src: z.union([image(), z.string().url()]),
+        alt: z.string(),
+      }),
+
+      gallery: z
+        .array(
+          z.object({
+            src: z.union([image(), z.string().url()]),
+            alt: z.string(),
+            caption: z.string().optional(),
+          }),
+        )
+        .default([]),
+
+      publishDate: z.string().transform((str) => new Date(str)),
+      author: z.string().default("Astroship"),
+      category: z.string(),
+      tags: z.array(z.string()),
     }),
-    publishDate: z.string().transform((str) => new Date(str)),
-    author: z.string().default("Astroship"),
-    category: z.string(),
-    tags: z.array(z.string()),
-  }),
 });
 
 const teamCollection = defineCollection({
@@ -36,10 +49,20 @@ const locationCollection = defineCollection({
       draft: z.boolean(),
       name: z.string(),
       title: z.string(),
+
+      attribution: z
+      .object({
+        creator: z.string(),
+        source: z.string(),
+        url: z.string().url(),
+      })
+      .optional(),
+
       avatar: z.object({
         src: z.union([image(), z.string().url()]),
         alt: z.string(),
       }),
+
       publishDate: z.string().transform((str) => new Date(str)),
     }),
 });
